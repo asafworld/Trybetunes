fafad6620ef3c01ccaa28c24e435bf9efd1b305f
asafworld <gabrielasaf10@gmail.com>
2022-04-18 15:55:19 -0300

    Requisito 1

---
+++ src/pages/Album.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    // const { id } = this.props.match.params
+    return (
+      <div data-testid="page-album" />
+    );
+  }
+}
+
+export default Album;
