471b871ae26180609b84eae86abf7839e6918425
asafworld <gabrielasaf10@gmail.com>
2022-04-18 19:30:58 -0300

    Tenta realizar o teste novamente

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-      <div data-testid="page-not-found" />
+      <div data-testid="page-not-found">
+        <h1>Not Found</h1>
+      </div>
     );
   }
 }
