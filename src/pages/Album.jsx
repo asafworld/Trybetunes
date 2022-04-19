1f586e6afff869c45ab7251b0de0cf54642544d6
asafworld <gabrielasaf10@gmail.com>
2022-04-19 11:20:38 -0300

    Adiciona o requisito 6

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Album extends React.Component {
   render() {
-    // const { id } = this.props.match.params
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    console.log(id);
     return (
       <div data-testid="page-album">
         <Header />
@@ -12,5 +16,13 @@ class Album extends React.Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default Album;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       searchValue: '',
+      inputLastValue: '',
       disabled: true,
+      loading: false,
+      resultFull: false,
+      resultEmpty: false,
+      artist: [],
     };
     this.buttonAble = this.buttonAble.bind(this);
     this.searchInputChange = this.searchInputChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  search = async () => {
+    const { searchValue } = this.state;
+    this.setState({ loading: true, inputLastValue: searchValue });
+    const albumArray = await searchAlbumsAPI(searchValue);
+    if (albumArray.length === 0) {
+      this.setState({
+        searchValue: '',
+        loading: false,
+        resultEmpty: true,
+        artist: albumArray });
+    }
+    this.setState({
+      searchValue: '',
+      loading: false,
+      resultFull: true,
+      artist: albumArray });
   }
 
   buttonAble() {
@@ -26,29 +53,70 @@ class Search extends React.Component {
   }
 
   render() {
-    const { disabled, searchValue } = this.state;
+    const {
+      disabled,
+      searchValue,
+      loading,
+      resultEmpty,
+      resultFull,
+      artist,
+      inputLastValue } = this.state;
+    console.log(artist);
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
-        <form>
-          <label htmlFor="search-artist-input">
-            <input
-              data-testid="search-artist-input"
-              id="search-artist-input"
-              value={ searchValue }
-              placeholder="Typer your artist or album"
-              onChange={ this.searchInputChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ disabled }
-          >
-            Search
-          </button>
-        </form>
+        { loading ? <Loading /> : (
+          <form>
+            <label htmlFor="search-artist-input">
+              <input
+                data-testid="search-artist-input"
+                id="search-artist-input"
+                value={ searchValue }
+                placeholder="Typer your artist or album"
+                onChange={ this.searchInputChange }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              onClick={ this.search }
+              disabled={ disabled }
+            >
+              Search
+            </button>
+          </form>)}
+        { resultEmpty && (<h2>Nenhum álbum foi encontrado</h2>) }
+
+        { resultFull && (
+          <section>
+            <h2>
+              Resultado de álbuns de:
+              {' '}
+              { inputLastValue }
+            </h2>
+            <ul>
+              { artist.map((album) => {
+                const { collectionId,
+                  collectionName,
+                  artistName,
+                  artworkUrl100 } = album;
+                const lines = (
+                  <li key={ collectionId }>
+                    <img src={ artworkUrl100 } alt={ `${collectionName} art cover` } />
+                    <Link
+                      to={ `/album/${collectionId}` }
+                      data-testid={ `link-to-album-${collectionId}` }
+                    >
+                      { collectionName }
+                    </Link>
+                    <h4>{ artistName }</h4>
+                  </li>);
+                return lines;
+              })}
+            </ul>
+          </section>
+        )}
       </div>
     );
   }
